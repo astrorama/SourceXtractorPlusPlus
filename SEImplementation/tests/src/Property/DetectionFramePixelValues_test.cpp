07b77648b471d1b698b820588e5f42a30a31e434
mschefer <mschefer@ac036500-4c5e-4199-b755-dd1b844d1672>
2016-06-30 11:35:45 +0000

    Added some unit tests

--- SEImplementation/tests/src/Property/DetectionFramePixelValues_test.cpp
+++ SEImplementation/tests/src/Property/DetectionFramePixelValues_test.cpp
@@ -6,9 +6,21 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "SEFramework/Source/Source.h"
+#include "SEFramework/Task/TaskRegistry.h"
+#include "SEFramework/Image/VectorImage.h"
+
 #include "SEImplementation/Property/DetectionFramePixelValues.h"
+#include "SEImplementation/Task/DetectionFramePixelValuesTask.h"
 
 using namespace SExtractor;
+
+struct DetectionFramePixelValuesFixture {
+  std::shared_ptr<TaskRegistry> task_registry;
+  std::shared_ptr<Source> source;
+
+  DetectionFramePixelValuesFixture() : task_registry(new TaskRegistry) {}
+};
 
 //-----------------------------------------------------------------------------
 
@@ -16,10 +28,18 @@ BOOST_AUTO_TEST_SUITE (DetectionFramePixelValues_test)
 
 //-----------------------------------------------------------------------------
 
-BOOST_AUTO_TEST_CASE( example_test ) {
+BOOST_FIXTURE_TEST_CASE(example_test, DetectionFramePixelValuesFixture) {
+  source.reset(new Source(
+      std::vector<PixelCoordinate>( { PixelCoordinate(2,0), PixelCoordinate(1,1) } ), task_registry));
+  BOOST_CHECK(source->getPixels() == std::vector<PixelCoordinate>( { PixelCoordinate(2,0), PixelCoordinate(1,1) } ));
 
-  //BOOST_FAIL("!!!! Please implement your tests !!!!");
+  auto image = std::make_shared<VectorImage<double>>(3, 2, std::vector<double>{0.0, 1.0, 2.0, 3.0, 4.0, 5.0});
 
+  DetectionFramePixelValuesTask task(image);
+  task.computeProperties(*source);
+
+  auto& pixel_values = source->getProperty<DetectionFramePixelValues>();
+  BOOST_CHECK(pixel_values.getValues() == std::vector<double>( {2.0, 4.0} ));
 }
 
 //-----------------------------------------------------------------------------
--- SEImplementation/tests/src/Property/DetectionFrameSourceStamp_test.cpp
+++ SEImplementation/tests/src/Property/DetectionFrameSourceStamp_test.cpp
@@ -6,9 +6,24 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "SEFramework/Source/Source.h"
+#include "SEFramework/Task/TaskRegistry.h"
+#include "SEFramework/Image/VectorImage.h"
+
 #include "SEImplementation/Property/DetectionFrameSourceStamp.h"
+#include "SEImplementation/Task/DetectionFrameSourceStampTask.h"
+#include "SEImplementation/Task/PixelBoundariesTaskFactory.h"
 
 using namespace SExtractor;
+
+struct DetectionFrameSourceStampFixture {
+  std::shared_ptr<TaskRegistry> task_registry;
+  std::shared_ptr<Source> source;
+
+  DetectionFrameSourceStampFixture() : task_registry(new TaskRegistry) {
+    task_registry->registerTaskFactory(std::unique_ptr<TaskFactory>(new PixelBoundariesTaskFactory));
+  }
+};
 
 //-----------------------------------------------------------------------------
 
@@ -16,13 +31,27 @@ BOOST_AUTO_TEST_SUITE (DetectionFrameSourceStamp_test)
 
 //-----------------------------------------------------------------------------
 
-BOOST_AUTO_TEST_CASE( example_test ) {
+BOOST_FIXTURE_TEST_CASE(example_test, DetectionFrameSourceStampFixture) {
+  source.reset(new Source(
+      std::vector<PixelCoordinate>( { PixelCoordinate(2,0), PixelCoordinate(1,1) } ), task_registry));
+  BOOST_CHECK(source->getPixels() == std::vector<PixelCoordinate>( { PixelCoordinate(2,0), PixelCoordinate(1,1) } ));
 
-  //BOOST_FAIL("!!!! Please implement your tests !!!!");
+  auto image = std::make_shared<VectorImage<double>>(3, 2, std::vector<double>{0.0, 1.0, 2.0, 3.0, 4.0, 5.0});
 
+  DetectionFrameSourceStampTask task(image);
+  task.computeProperties(*source);
+
+  auto& source_stamp = source->getProperty<DetectionFrameSourceStamp>().getStamp();
+
+  BOOST_CHECK(source_stamp.getWidth() == 2);
+  BOOST_CHECK(source_stamp.getHeight() == 2);
+  BOOST_CHECK_CLOSE(source_stamp.getValue(0,0), 1.0, 0.000001);
+  BOOST_CHECK_CLOSE(source_stamp.getValue(1,0), 2.0, 0.000001);
+  BOOST_CHECK_CLOSE(source_stamp.getValue(0,1), 4.0, 0.000001);
+  BOOST_CHECK_CLOSE(source_stamp.getValue(1,1), 5.0, 0.000001);
 }
 
-//-----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 
 BOOST_AUTO_TEST_SUITE_END ()
 
