3bde2edb5fa26ee210689c0ea7a6bfa856b7d23d
Nikolaos Apostolakos <22575160+nikoapos@users.noreply.github.com>
2016-07-21 16:26:56 +0000

    Added a check to the OutputConfig class to validate that the user didn't gave the empty string

--- SEImplementation/src/lib/Configuration/OutputConfig.cpp
+++ SEImplementation/src/lib/Configuration/OutputConfig.cpp
@@ -22,9 +22,18 @@ OutputConfig::OutputConfig(long manager_id) : Configuration(manager_id) {
 
 std::map<std::string, Configuration::OptionDescriptionList> OutputConfig::getProgramOptions() {
   return { {"Output columns", {
-      {OUTPUT_COLUMNS.c_str(), po::value<std::string>()->default_value(""),
+      {OUTPUT_COLUMNS.c_str(), po::value<std::string>()->required(),
           "Comma-separated list of output columns"},
   }}};
+}
+
+void OutputConfig::preInitialize(const UserValues& args) {
+  // In pre initialize phase we do a fast check that the use gave at least one
+  // column name
+  if (args.at(OUTPUT_COLUMNS).as<std::string>().size() == 0) {
+    throw Elements::Exception() << OUTPUT_COLUMNS << " option must contain at "
+            << "least one column name";
+  }
 }
 
 void OutputConfig::initialize(const UserValues& args) {
@@ -33,7 +42,6 @@ void OutputConfig::initialize(const UserValues& args) {
   boost::char_separator<char> separator(",");
   boost::tokenizer<boost::char_separator<char>> tok(output_columns_string, separator);
 
-  std::vector<std::string> output_columns_names;
   std::copy(tok.begin(), tok.end(), back_inserter(m_output_columns));
 }
 
