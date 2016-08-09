610033f17c850d788059bebe9bd2f4ac7c11caed
nikoapos <nikoapos@ac036500-4c5e-4199-b755-dd1b844d1672>
2016-08-09 09:53:54 +0000

    Added the SimpleSourceGroupFactory

---
+++ trunk/SEFramework/SEFramework/Source/SimpleSourceGroupFactory.h
@@ -0,0 +1,27 @@
+/* 
+ * @file SimpleSourceGroupFactory.h
+ * @author nikoapos
+ */
+
+#ifndef _SEFRAMEWORK_SIMPLESOURCEGROUPFACTORY_H
+#define _SEFRAMEWORK_SIMPLESOURCEGROUPFACTORY_H
+
+#include "SEFramework/Source/SourceGroupFactory.h"
+#include "SEFramework/Source/SimpleSourceGroup.h"
+
+namespace SExtractor {
+
+class SimpleSourceGroupFactory : public SourceGroupFactory {
+  
+public:
+
+  virtual std::shared_ptr<SourceGroupInterface> createSourceGroup() const override {
+    return std::make_shared<SimpleSourceGroupFactory>();
+  }
+  
+};
+
+}
+
+#endif /* _SEFRAMEWORK_SIMPLESOURCEGROUPFACTORY_H */
+
