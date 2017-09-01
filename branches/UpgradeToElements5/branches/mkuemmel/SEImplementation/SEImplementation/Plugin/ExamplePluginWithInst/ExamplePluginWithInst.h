04c7d5db07afdb75095e4383c00c308ee68828e8
nikoapos <nikoapos@ac036500-4c5e-4199-b755-dd1b844d1672>
2017-09-01 08:11:14 +0000

    Removing the UpgradeToElements5 branch because the whole tree was copied instead of only the trunk ny mistake

--- branches/UpgradeToElements5/branches/mkuemmel/SEImplementation/SEImplementation/Plugin/ExamplePluginWithInst/ExamplePluginWithInst.h
+++
@@ -1,9 +0,0 @@
-#include "SEFramework/Plugin/Plugin.h"
-namespace SExtractor {
-class ExamplePluginWithInst : public Plugin {
-public:
-  virtual ~ExamplePluginWithInst() = default;
-  virtual void registerPlugin(PluginAPI& plugin_api) override;
-  virtual std::string getIdString() const override;
-}; // End of TestPlugin class
-}
--- branches/UpgradeToElements5/python-proof-of-concept/detection/pixelproperties.py
+++
@@ -1,5 +0,0 @@
-def getImageFlag(im):
-    return lambda x,y : im[y][x] > 0 if (x<len(im[0]) and y<len(im)) else False
-
-def getImageValue(im):
-    return lambda x,y : im[y][x]
