38473481098fb1a555d7d90aed96b00834099920
Marc Schefer <Marc.Schefer@unige.ch>
2017-12-12 14:52:14 +0100

    added missing files

---
+++ SEImplementation/SEImplementation/CheckImages/SourceIdCheckImage.h
@@ -0,0 +1,36 @@
+/*
+ * SegmentationCheckImage.h
+ *
+ *  Created on: Dec 11, 2017
+ *      Author: mschefer
+ */
+
+#ifndef _SEIMPLEMENTATION_CHECKIMAGES_SEGMENTATIONCHECKIMAGE_H_
+#define _SEIMPLEMENTATION_CHECKIMAGES_SEGMENTATIONCHECKIMAGE_H_
+
+#include "SEUtils/Observable.h"
+#include "SEFramework/Image/VectorImage.h"
+#include "SEFramework/Source/SourceInterface.h"
+
+namespace SExtractor {
+
+class SourceIdCheckImage : public Observer<std::shared_ptr<SourceInterface>> {
+public:
+
+  SourceIdCheckImage(std::shared_ptr<VectorImage<unsigned int>> check_image) :
+      m_check_image(check_image),
+      m_object_id(1)
+  {}
+
+  virtual void handleMessage(const std::shared_ptr<SourceInterface>& source);
+
+private:
+  std::shared_ptr<VectorImage<unsigned int>> m_check_image;
+  unsigned int m_object_id;
+
+};
+
+}
+
+
+#endif /* SEIMPLEMENTATION_SEIMPLEMENTATION_CHECKIMAGES_SEGMENTATIONCHECKIMAGE_H_ */
---
+++ SEImplementation/src/lib/CheckImages/SourceIdCheckImage.cpp
@@ -0,0 +1,21 @@
+#include "SEImplementation/CheckImages/SourceIdCheckImage.h"
+
+#include "SEImplementation/Property/PixelCoordinateList.h"
+
+#include "SEImplementation/CheckImages/CheckImages.h"
+
+
+namespace SExtractor {
+
+void SourceIdCheckImage::handleMessage(const std::shared_ptr<SourceInterface>& source) {
+  if (m_check_image) {
+    auto coordinates = source->getProperty<PixelCoordinateList>();
+
+    for (auto& coord : coordinates.getCoordinateList()) {
+      m_check_image->setValue(coord, m_object_id);
+    }
+    m_object_id++;
+  }
+}
+
+}
