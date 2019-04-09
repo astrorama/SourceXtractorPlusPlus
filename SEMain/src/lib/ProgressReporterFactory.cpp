b78f6b15a6a886f1fb09267bc1294029c9211ce6
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2019-04-09 10:39:33 +0200

    Disable progress bar if catalog written to stdout

--- SEMain/src/lib/ProgressReporterFactory.cpp
+++ SEMain/src/lib/ProgressReporterFactory.cpp
@@ -1,3 +1,4 @@
+#include "SEImplementation/Configuration/OutputConfig.h"
 #include "SEMain/ProgressReporterFactory.h"
 #include "SEMain/ProgressReporterConfiguration.h"
 #include "SEMain/ProgressLogger.h"
@@ -9,13 +10,19 @@ ProgressReporterFactory::ProgressReporterFactory(): m_disable_progress_bar{false
 
 void ProgressReporterFactory::reportConfigDependencies(Euclid::Configuration::ConfigManager& manager) const {
   manager.registerConfiguration<ProgressReporterConfiguration>();
+  manager.registerConfiguration<OutputConfig>();
 }
 
 void ProgressReporterFactory::configure(Euclid::Configuration::ConfigManager& manager) {
   auto progress_config = manager.getConfiguration<ProgressReporterConfiguration>();
+  auto output_config = manager.getConfiguration<OutputConfig>();
   m_min_interval = progress_config.getMinPrintInterval();
   m_disable_progress_bar = progress_config.isProgressBarDisabled();
   m_log_file = progress_config.isLogFileSet();
+  // If the output is written to stdout, we can't use the terminal for the fancy ncurses interface
+  if (output_config.getOutputFile().empty()) {
+    m_disable_progress_bar = true;
+  }
 }
 
 std::shared_ptr<ProgressMediator> ProgressReporterFactory::createProgressMediator() const {
@@ -27,7 +34,7 @@ std::shared_ptr<ProgressMediator> ProgressReporterFactory::createProgressMediato
     mediator->ProgressObservable::addObserver(progress_bar);
     mediator->DoneObservable::addObserver(progress_bar);
   }
-  if (!::isatty(::fileno(stderr)) || m_log_file) {
+  if (!::isatty(::fileno(stderr)) || m_log_file || m_disable_progress_bar) {
 #endif
     auto logger = std::make_shared<ProgressLogger>(m_min_interval);
     mediator->ProgressObservable::addObserver(logger);
