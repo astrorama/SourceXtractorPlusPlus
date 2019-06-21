704ddd3df1da621dea83a048a648cbe94699e04f
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2019-06-21 13:44:50 +0200

    Added InterpolatedImageSource

---
+++ SEFramework/SEFramework/Image/InterpolatedImageSource.h
@@ -0,0 +1,88 @@
+/*
+ * InterpolatedImageSource.h
+ *
+ *  Created on: Jun 21, 2019
+ *      Author: Alejandro Alvarez Ayllon
+ */
+
+#ifndef SEFRAMEWORK_SEFRAMEWORK_IMAGE_INTERPOLATEDIMAGESOURCE_H_
+#define SEFRAMEWORK_SEFRAMEWORK_IMAGE_INTERPOLATEDIMAGESOURCE_H_
+
+#include "SEFramework/Image/ProcessingImageSource.h"
+
+namespace SExtractor {
+
+/**
+ * Interpolates an image using a variance map: substitute "bad" pixels with the nearest "good" one
+ */
+template<typename T>
+class InterpolatedImageSource : public ProcessingImageSource<T> {
+public:
+  InterpolatedImageSource(std::shared_ptr<Image<T>> image, std::shared_ptr<WeightImage> variance_map,
+                          WeightImage::PixelType variance_threshold, int interpolation_gap)
+    : ProcessingImageSource<T>(image),
+      m_variance_map(variance_map), m_variance_threshold(variance_threshold),
+      m_interpolation_gap(interpolation_gap) {
+  }
+
+  std::string getRepr() const override {
+    return "InterpolatedImageSource(" + getImageRepr() + "," + m_variance_map->getRepr() + ")";
+  }
+
+protected:
+  using ProcessingImageSource<T>::getImageRepr;
+
+  void generateTile(std::shared_ptr<Image<T>> image, ImageTile<T>& tile,
+                    int x, int y, int width, int height) const override {
+    // Get the chunks we are interested in, and its surrounding area so we can convolve
+    auto chunk_start_x = x - m_interpolation_gap;
+    auto chunk_end_x = x + width;
+    auto chunk_start_y = y - m_interpolation_gap;
+    auto chunk_end_y = y + height;
+
+    // Remember to clip to avoid accessing the image out of bounds!
+    auto chunk_pixel_x = std::max(chunk_start_x, 0);
+    auto chunk_pixel_y = std::max(chunk_start_y, 0);
+    auto chunk_w = std::min(chunk_end_x - chunk_pixel_x, image->getWidth() - chunk_pixel_x);
+    auto chunk_h = std::min(chunk_end_y - chunk_pixel_y, image->getHeight() - chunk_pixel_y);
+
+    // Get the chunks for the image and the variance
+    // We rely on the underlying chain to generate them efficiently (i.e. BufferedImage across tiles)
+    auto img_chunk = image->getChunk(chunk_pixel_x, chunk_pixel_y, chunk_w, chunk_h);
+    auto variance_chunk = m_variance_map->getChunk(chunk_pixel_x, chunk_pixel_y, chunk_w, chunk_h);
+
+    // Fill the tile interpolating from the chunks
+    int off_x = x - chunk_pixel_x;
+    int off_y = y - chunk_pixel_y;
+    auto& tile_data = *tile.getImage();
+    for (int iy = 0; iy < height; ++iy) {
+      for (int ix = 0; ix < width; ++ix) {
+        tile_data.at(ix, iy) = getInterpolatedValue(*img_chunk, *variance_chunk, ix + off_x, iy + off_y);
+      }
+    }
+  }
+
+private:
+  std::shared_ptr<WeightImage> m_variance_map;
+  WeightImage::PixelType m_variance_threshold;
+  int m_interpolation_gap;
+
+  inline T getInterpolatedValue(const ImageChunk<T>& img, const ImageChunk<T>& var, int x, int y) const {
+    if (var.getValue(x, y) < m_variance_threshold)
+      return img.getValue(x, y);
+
+    for (int i = 1; i <= m_interpolation_gap; i++) {
+      if (x - i >= 0 && var.getValue(x - i, y) < m_variance_threshold) {
+        return img.getValue(x - i, y);
+      }
+      if (y - i >= 0 && var.getValue(x, y - i) < m_variance_threshold) {
+        return img.getValue(x, y - i);
+      }
+    }
+    return img.getValue(x, y);
+  }
+};
+
+} // end namespace SExtractor
+
+#endif
--- SEFramework/tests/src/Image/InterpolatedImageSource_test.cpp
+++ SEFramework/tests/src/Image/InterpolatedImageSource_test.cpp
@@ -1,12 +1,13 @@
 /**
- * @file tests/src/InterpolatedImage_test.cpp
+ * @file tests/src/InterpolatedImageSource_test.cpp
  * @date 21/06/19
  * @author Alejandro Alvarez Ayllon
  */
 
 #include <boost/test/unit_test.hpp>
+#include "SEFramework/Image/BufferedImage.h"
 #include "SEFramework/Image/ConstantImage.h"
-#include "SEFramework/Image/InterpolatedImage.h"
+#include "SEFramework/Image/InterpolatedImageSource.h"
 #include "SEFramework/Image/VectorImage.h"
 #include "SEUtils/CompareImages.h"
 
@@ -25,14 +26,21 @@ struct InterpolatedImageSourceFixture {
   }
 };
 
+template<typename T, typename ...Args>
+std::shared_ptr<BufferedImage<T>> createBufferedHelper(Args&& ...args) {
+  return BufferedImage<T>::create(
+    std::shared_ptr<InterpolatedImageSource<T>>(new InterpolatedImageSource<T>(std::forward<Args>(args)...))
+  );
+}
+
 //-----------------------------------------------------------------------------
 
-BOOST_AUTO_TEST_SUITE (InterpolatedImage_test)
+BOOST_AUTO_TEST_SUITE (InterpolatedImageSource_test)
 
 //-----------------------------------------------------------------------------
 
 BOOST_FIXTURE_TEST_CASE (InterpolatedImage_all_good_test, InterpolatedImageSourceFixture) {
-  auto interpolated = InterpolatedImage<SeFloat>::create(m_image, ConstantImage<SeFloat>::create(4, 4, 1.), 10., 1);
+  auto interpolated = createBufferedHelper<SeFloat>(m_image, ConstantImage<SeFloat>::create(4, 4, 1.), 10., 1);
   BOOST_CHECK(compareImages(interpolated, m_image));
 }
 
@@ -45,7 +53,7 @@ BOOST_FIXTURE_TEST_CASE (InterpolatedImage_bad_column_test, InterpolatedImageSou
     1., 1., 5., 1.,
     1., 1., 5., 1.,
   });
-  auto interpolated = InterpolatedImage<SeFloat>::create(m_image, variance, 2., 1);
+  auto interpolated = createBufferedHelper<SeFloat>(m_image, variance, 2., 1);
   auto expected = VectorImage<SeFloat>::create(4, 4, std::vector<SeFloat>{
     11., 12., 12., 14.,
     21., 22., 22., 24.,
@@ -64,7 +72,7 @@ BOOST_FIXTURE_TEST_CASE (InterpolatedImage_bad_first_column_test, InterpolatedIm
     5., 1., 1., 1.,
     5., 1., 1., 1.,
   });
-  auto interpolated = InterpolatedImage<SeFloat>::create(m_image, variance, 2., 1);
+  auto interpolated = createBufferedHelper<SeFloat>(m_image, variance, 2., 1);
   auto expected = VectorImage<SeFloat>::create(4, 4, std::vector<SeFloat>{
     11., 12., 13., 14.,
     21., 22., 23., 24.,
@@ -83,7 +91,7 @@ BOOST_FIXTURE_TEST_CASE (InterpolatedImage_bad_wide_column_test, InterpolatedIma
     1., 5., 5., 1.,
     1., 5., 5., 1.,
   });
-  auto interpolated = InterpolatedImage<SeFloat>::create(m_image, variance, 2., 2);
+  auto interpolated = createBufferedHelper<SeFloat>(m_image, variance, 2., 2);
   auto expected = VectorImage<SeFloat>::create(4, 4, std::vector<SeFloat>{
     11., 11., 11., 14.,
     21., 21., 21., 24.,
@@ -102,7 +110,7 @@ BOOST_FIXTURE_TEST_CASE (InterpolatedImage_bad_row_test, InterpolatedImageSource
     5., 5., 5., 5.,
     1., 1., 1., 1.,
   });
-  auto interpolated = InterpolatedImage<SeFloat>::create(m_image, variance, 2., 1);
+  auto interpolated = createBufferedHelper<SeFloat>(m_image, variance, 2., 1);
   auto expected = VectorImage<SeFloat>::create(4, 4, std::vector<SeFloat>{
     11., 12., 13., 14.,
     21., 22., 23., 24.,
@@ -121,7 +129,7 @@ BOOST_FIXTURE_TEST_CASE (InterpolatedImage_bad_first_row_test, InterpolatedImage
     1., 1., 1., 1.,
     1., 1., 1., 1.,
   });
-  auto interpolated = InterpolatedImage<SeFloat>::create(m_image, variance, 2., 1);
+  auto interpolated = createBufferedHelper<SeFloat>(m_image, variance, 2., 1);
   auto expected = VectorImage<SeFloat>::create(4, 4, std::vector<SeFloat>{
     11., 12., 13., 14.,
     21., 22., 23., 24.,
@@ -140,7 +148,7 @@ BOOST_FIXTURE_TEST_CASE (InterpolatedImage_bad_wide_row_test, InterpolatedImageS
     5., 5., 5., 5.,
     1., 1., 1., 1.,
   });
-  auto interpolated = InterpolatedImage<SeFloat>::create(m_image, variance, 2., 2);
+  auto interpolated = createBufferedHelper<SeFloat>(m_image, variance, 2., 2);
   auto expected = VectorImage<SeFloat>::create(4, 4, std::vector<SeFloat>{
     11., 12., 13., 14.,
     11., 12., 13., 14.,
@@ -159,7 +167,7 @@ BOOST_FIXTURE_TEST_CASE (InterpolatedImage_bad_no_gap_test, InterpolatedImageSou
     1., 1., 5., 1.,
     1., 1., 1., 1.,
   });
-  auto interpolated = InterpolatedImage<SeFloat>::create(m_image, variance, 2., 0);
+  auto interpolated = createBufferedHelper<SeFloat>(m_image, variance, 2., 0);
   auto expected = VectorImage<SeFloat>::create(4, 4, std::vector<SeFloat>{
     11., 12., 13., 14.,
     21., 22., 23., 24.,
@@ -167,6 +175,84 @@ BOOST_FIXTURE_TEST_CASE (InterpolatedImage_bad_no_gap_test, InterpolatedImageSou
     41., 42., 43., 44.
   });
   BOOST_CHECK(compareImages(interpolated, expected));
+}
+
+//-----------------------------------------------------------------------------
+
+template <typename T>
+class InterpolatedImageSourceWrapper: public InterpolatedImageSource<T> {
+  public:
+    mutable int m_tile_count = 0;
+
+    using InterpolatedImageSource<T>::InterpolatedImageSource;
+
+  protected:
+  void generateTile(std::shared_ptr<Image<T>> image, ImageTile<T>& tile,
+                    int x, int y, int width, int height) const override {
+    ++m_tile_count;
+    InterpolatedImageSource<T>::generateTile(image, tile, x, y, width, height);
+  }
+};
+
+struct InterpolatedImageSourceTileFixture {
+  std::shared_ptr<VectorImage<SeFloat>> m_image, m_variance, m_expected;
+  std::shared_ptr<BufferedImage<SeFloat>> m_buffered;
+  std::shared_ptr<TileManager> m_tile_manager;
+  std::shared_ptr<InterpolatedImageSourceWrapper<SeFloat>> m_interpolated_source;
+
+  InterpolatedImageSourceTileFixture() {
+    m_image = VectorImage<SeFloat>::create(8, 8, std::vector<SeFloat>{
+      11., 12., 13., 14.,    15., 16., 17., 18.,
+      21., 22., 23., 24.,    25., 26., 27., 28.,
+      31., 32., 33., 34.,    35., 36., 37., 38.,
+      41., 42., 43., 44.,    45., 46., 47., 48.,
+
+      51., 52., 53., 54.,    55., 56., 57., 58.,
+      61., 62., 63., 64.,    65., 66., 67., 68.,
+      71., 72., 73., 74.,    75., 76., 77., 78.,
+      81., 82., 83., 84.,    85., 86., 87., 88.,
+    });
+    m_variance = VectorImage<SeFloat>::create(8, 8, std::vector<SeFloat>{
+      5., 5., 5., 5.,    5., 0., 0., 0.,
+      5., 0., 0., 0.,    5., 0., 0., 0.,
+      5., 0., 0., 0.,    5., 0., 0., 0.,
+      5., 0., 0., 0.,    5., 0., 0., 0.,
+
+      0., 0., 0., 0.,    5., 5., 5., 5.,
+      0., 0., 0., 5.,    0., 0., 5., 0.,
+      0., 0., 0., 5.,    0., 0., 5., 0.,
+      0., 0., 0., 0.,    0., 0., 5., 0.,
+    });
+    m_expected = VectorImage<SeFloat>::create(8, 8, std::vector<SeFloat>{
+      11., 12., 13., 14.,     15., 16., 17., 18.,
+      21., 22., 23., 24.,     24., 26., 27., 28.,
+      31., 32., 33., 34.,     34., 36., 37., 38.,
+      41., 42., 43., 44.,     44., 46., 47., 48.,
+
+      51., 52., 53., 54.,     54., 46., 47., 48.,
+      61., 62., 63., 63.,     65., 66., 66., 68.,
+      71., 72., 73., 73.,     75., 76., 76., 78.,
+      81., 82., 83., 84.,     85., 86., 86., 88.,
+    });
+    m_tile_manager = std::make_shared<TileManager>();
+    m_tile_manager->setOptions(4, 4, 1024);
+    m_interpolated_source = std::make_shared<InterpolatedImageSourceWrapper<SeFloat>>(m_image, m_variance, 2., 1);
+    m_buffered = BufferedImage<SeFloat>::create(m_interpolated_source, m_tile_manager);
+  }
+};
+
+template<typename T, typename ...Args>
+std::shared_ptr<BufferedImage<T>> createBufferedHelper(Args&& ...args) {
+  return BufferedImage<T>::create(
+    std::shared_ptr<InterpolatedImageSource<T>>(new InterpolatedImageSource<T>(std::forward<Args>(args)...))
+  );
+}
+
+//-----------------------------------------------------------------------------
+
+BOOST_FIXTURE_TEST_CASE (InterpolatedImageSource_tile_full, InterpolatedImageSourceTileFixture) {
+  BOOST_CHECK(compareImages(m_expected, m_buffered));
+  BOOST_CHECK_EQUAL(m_interpolated_source->m_tile_count, 4);
 }
 
 //-----------------------------------------------------------------------------
