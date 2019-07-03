d9f19be88c19f70c49554fb9185014c8b70753b1
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2019-07-03 09:35:43 +0200

    Add util to compare collection of floating point

--- SEUtils/SEUtils/CompareImages.h
+++ SEUtils/SEUtils/CompareImages.h
@@ -8,7 +8,6 @@
 #ifndef _COMPAREIMAGES_H
 #define _COMPAREIMAGES_H
 
-#include "SEFramework/Image/Image.h"
 #include "SEUtils/IsClose.h"
 
 namespace SExtractor {
@@ -28,6 +27,35 @@ boost::test_tools::predicate_result compareImages(
                       << ": " << expected << " != " << value << "\n";
       }
     }
+  }
+
+  return res;
+}
+
+template <typename T, typename U>
+boost::test_tools::predicate_result compareCollections(
+  const T& ref, const U& val, double atol = 1e-8, double rtol = 1e-5) {
+  boost::test_tools::predicate_result res(true);
+
+  auto ref_i = std::begin(ref);
+  auto val_i = std::begin(val);
+  int i = 0;
+
+  while (ref_i != std::end(ref) && val_i != std::end(val)) {
+
+    if (!isClose(*ref_i, *val_i, atol, rtol)) {
+      res = false;
+      res.message() << "Not matching values at position " << i << ": " << *ref_i << " != " << *val_i << "\n";
+    }
+
+    ++ref_i;
+    ++val_i;
+    ++i;
+  }
+
+  if (ref_i != std::end(ref) || val_i != std::end(val)) {
+    res = false;
+    res.message() << "The sequences have different length!" << "\n";
   }
 
   return res;
