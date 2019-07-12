fad87f486008f12b54d1df9df00c99795f99763c
“Martin <mkuemmel@usm.lmu.de>
2019-07-12 15:45:42 +0200

    Put in some timings for computing the stacked PSF's.

--- SEImplementation/src/lib/Plugin/VariablePsf/PsfPluginConfig.cpp
+++ SEImplementation/src/lib/Plugin/VariablePsf/PsfPluginConfig.cpp
@@ -5,6 +5,8 @@
  *      Author: Alejandro ��lvarez Ayll��n (greatly adapted from mschefer's code)
  */
 
+#include <ctime>
+
 #include <Configuration/ProgramOptionsHelper.h>
 #include <SEImplementation/Image/ImagePsf.h>
 #include <CCfits/CCfits>
@@ -32,15 +34,32 @@ static const std::string PSF_PIXELSCALE {"psf-pixel-scale" };
  */
 static std::shared_ptr<VariablePsf> readStackedPsf(std::unique_ptr<CCfits::FITS> &pFits) {
 
+  srand (static_cast <unsigned> (time(0)));
+
   // read in the stacked file;
   // extract one PSF;
   // write to a FITS file;
   std::shared_ptr<VectorImage<SeFloat>> act_psf;
   std::vector<double> pos_vector;
   std::shared_ptr<VariablePsfStack> act_stack = std::make_shared<VariablePsfStack>(std::move(pFits));
-  pos_vector = {900.0, 1300.0};
-  act_psf = act_stack->getPsf(pos_vector);
-  FitsWriter::writeFile(*act_psf, "mypsf_1.fits");
+
+  for (int counter=0; counter < 10000; counter++){
+    double rand_x = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/10000.));
+    double rand_y = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/10000.));
+    pos_vector = {rand_x, rand_y};
+    logger.info() << pos_vector[0] << " " << pos_vector[1];
+    act_psf = act_stack->getPsf(pos_vector);
+    double sum=0.;
+    for (int ixx=0; ixx < act_psf->getWidth(); ixx++){
+      for (int iyy=0; iyy < act_psf->getHeight(); iyy++){
+        sum += act_psf->getValue(ixx, iyy);
+      }
+    }
+    logger.info() << "Sum: " << sum;
+    //FitsWriter::writeFile(*act_psf, "mypsf_1.fits"); // somehow when storing all images in the identical file it is empty with 0.0 all
+  }
+
+  /*
   pos_vector = {15000.0, 4500.0};
   act_psf = act_stack->getPsf(pos_vector);
   FitsWriter::writeFile(*act_psf, "mypsf_2.fits");
@@ -56,6 +75,7 @@ static std::shared_ptr<VariablePsf> readStackedPsf(std::unique_ptr<CCfits::FITS>
   pos_vector = {11843.0, 1528.0};
   act_psf = act_stack->getPsf(pos_vector);
   FitsWriter::writeFile(*act_psf, "mypsf_6.fits");
+  */
 
   // thats kind of the emergency functionality
   return PsfPluginConfig::generateGaussianPsf(1.0, 0.1);
