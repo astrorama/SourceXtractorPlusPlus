c53322d5247103b39f667ed444b4c771f5e5904d
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2019-07-15 15:33:03 +0200

    Add docstrings to the C++ side

--- SEImplementation/src/lib/PythonConfig/PythonModule.cpp
+++ SEImplementation/src/lib/PythonConfig/PythonModule.cpp
@@ -18,15 +18,17 @@ namespace SExtractor {
 
 BOOST_PYTHON_MODULE(_SExtractorPy) {
   
-  bp::class_<ObjectInfo>("ObjectInfo", bp::init<SourceInterface&>())
-      .def("get_centroid_x", &ObjectInfo::getCentroidX)
-      .def("get_centroid_y", &ObjectInfo::getCentroidY)
-      .def("get_iso_flux", &ObjectInfo::getIsoFlux)
-      .def("get_radius", &ObjectInfo::getRadius)
-      .def("get_angle", &ObjectInfo::getAngle)
-      .def("get_aspect_ratio", &ObjectInfo::getAspectRatio);
+  bp::class_<ObjectInfo>("ObjectInfo",
+    "A source detected by SExtractor after the segmentation and deblending", bp::init<SourceInterface&>())
+      .def("get_centroid_x", &ObjectInfo::getCentroidX, "Get the X coordinate of the pixel centroid")
+      .def("get_centroid_y", &ObjectInfo::getCentroidY, "Get the Y coordinate of the pixel centroid")
+      .def("get_iso_flux", &ObjectInfo::getIsoFlux, "Get the isophotal flux")
+      .def("get_radius", &ObjectInfo::getRadius, "Get the source semi-major axis, in pixels")
+      .def("get_angle", &ObjectInfo::getAngle, "Get the source angle, in radians")
+      .def("get_aspect_ratio", &ObjectInfo::getAspectRatio, "Get the aspect ratio");
   
-  bp::class_<PyMeasurementImage>("MeasurementImage", bp::init<std::string, std::string, std::string>())
+  bp::class_<PyMeasurementImage>("MeasurementImage",
+    "C++ part of the MeasurementImage", bp::init<std::string, std::string, std::string>())
       .def_readonly("id", &PyMeasurementImage::id)
       .def_readonly("file", &PyMeasurementImage::file)
       .def_readwrite("gain", &PyMeasurementImage::gain)
@@ -43,29 +45,33 @@ BOOST_PYTHON_MODULE(_SExtractorPy) {
   bp::class_<PyId>("Id", bp::init<>())
     .def_readonly("id", &PyId::id);
 
-  bp::class_<PyAperture, bp::bases<PyId>>("Aperture", bp::init<bp::list>())
-    .def_readonly("apertures", &PyAperture::apertures)
+  bp::class_<PyAperture, bp::bases<PyId>>("Aperture",
+    "Set of aperture photometries", bp::init<bp::list>())
+    .def_readonly("apertures", &PyAperture::apertures, "List of aperture diameters, in pixels")
     .def("__str__", &PyAperture::toString)
     .def("__repr__", &PyAperture::toString);
 
-  bp::class_<CoordinateSystem, boost::noncopyable>("CoordinateSystem", bp::no_init)
+  bp::class_<CoordinateSystem, boost::noncopyable>("CoordinateSystem",
+    "Implements transformation of coordinates between image and world coordinates", bp::no_init)
       .def("image_to_world", &CoordinateSystem::imageToWorld)
       .def("world_to_image", &CoordinateSystem::worldToImage);
   bp::register_ptr_to_python<std::shared_ptr<CoordinateSystem>>();
 
-  bp::class_<WorldCoordinate>("WorldCoordinate")
+  bp::class_<WorldCoordinate>("WorldCoordinate", "World coordinates")
       .def(bp::init<double, double>())
       .def_readwrite("alpha", &WorldCoordinate::m_alpha)
       .def_readwrite("delta", &WorldCoordinate::m_delta);
 
-  bp::class_<ImageCoordinate>("ImageCoordinate")
+  bp::class_<ImageCoordinate>("ImageCoordinate", "Image coordinates, in pixels")
       .def(bp::init<double, double>())
       .def_readwrite("x", &ImageCoordinate::m_x)
       .def_readwrite("y", &ImageCoordinate::m_y);
 
   bp::class_<std::vector<double> >("_DoubleVector")
-    .def(bp::vector_indexing_suite<std::vector<double> >())
-  ;
+    .def(bp::vector_indexing_suite<std::vector<double> >());
+
+  bp::class_<std::vector<float> >("_FloatVector")
+    .def(bp::vector_indexing_suite<std::vector<float> >());
 }
 
 } // namespace SExtractor
