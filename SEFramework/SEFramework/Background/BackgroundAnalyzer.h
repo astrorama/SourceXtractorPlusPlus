bf3dfc9c076d6c41bf6042236ce9efaf44404f15
“Martin <mkuemmel@usm.lmu.de>
2019-11-21 16:39:08 +0100

    Some bugfix to leave the *background* map intact and only "flip" the variances.

--- SEFramework/SEFramework/Background/BackgroundAnalyzer.h
+++ SEFramework/SEFramework/Background/BackgroundAnalyzer.h
@@ -25,6 +25,7 @@
 #define _SEFRAMEWORK_BACKGROUND_BACKGROUNDANALYZER_H_
 
 #include "SEFramework/Frame/Frame.h"
+#include "SEFramework/Image/FunctionalImage.h"
 
 namespace SourceXtractor {
 
@@ -32,12 +33,17 @@ namespace SourceXtractor {
 class BackgroundModel {
 public:
 
-  BackgroundModel(std::shared_ptr<Image<SeFloat>> background_level, std::shared_ptr<Image<SeFloat>> background_variance,
-      SeFloat scaling_factor) :
-        m_background_level(background_level),
-        m_background_variance(background_variance),
-        m_scaling_factor(scaling_factor)
-      {}
+  BackgroundModel(std::shared_ptr<Image<SeFloat>> background_level, std::shared_ptr<Image<SeFloat>> background_variance, SeFloat scaling_factor) :
+    m_background_level(background_level),
+    m_scaling_factor(scaling_factor) {
+    // make sure the variance is a positive value
+    m_background_variance = FunctionalImage<SeFloat>::create(
+        background_variance->getWidth(), background_variance->getHeight(),
+        [background_variance](int x, int y) -> SeFloat {
+      return background_variance->getValue(x,y) < 0.0 ? -1.0*background_variance->getValue(x,y) : background_variance->getValue(x,y);;
+    }
+    );
+  }
 
   std::shared_ptr<Image<SeFloat>> getLevelMap() const {
     return m_background_level;
