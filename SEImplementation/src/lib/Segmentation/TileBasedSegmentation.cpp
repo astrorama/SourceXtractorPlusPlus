6ec68bd75e27dbc92ad73e255c3cfcd4f4010337
Marc Schefer <Marc.Schefer@unige.ch>
2020-04-20 12:51:57 +0200

    wip

--- SEImplementation/src/lib/Segmentation/TileBasedSegmentation.cpp
+++ SEImplementation/src/lib/Segmentation/TileBasedSegmentation.cpp
@@ -34,205 +34,139 @@ namespace {
 
 class TilesLabellingListener : public Lutz::LutzListener {
 public:
-  struct PartialGroup {
-    std::vector<PixelCoordinate> m_pixels;
-    std::unordered_set<PixelCoordinate> m_missing_tiles;
 
-    PartialGroup() : m_pixels(), m_missing_tiles() {}
-
-    PartialGroup(const std::vector<PixelCoordinate>& pixels, const std::unordered_set<PixelCoordinate>& missing_tiles={})
-        : m_pixels(pixels), m_missing_tiles(missing_tiles) {}
-
-    void determineMissingTiles(PixelCoordinate current_tile, int min_x, int min_y, int max_x, int max_y) {
-      bool left=false, right=false, bottom=false, top=false;
-      for (auto& pc : m_pixels) {
-          left   |= (pc.m_x <= min_x);
-          right  |= (pc.m_x >= max_x);
-          bottom |= (pc.m_y <= min_y);
-          top    |= (pc.m_y >= max_y);
-      }
-
-      if (left) {
-        m_missing_tiles.emplace(current_tile + PixelCoordinate(-1,0));
-      }
-      if (right) {
-        m_missing_tiles.emplace(current_tile + PixelCoordinate(1,0));
-      }
-      if (bottom) {
-        m_missing_tiles.emplace(current_tile + PixelCoordinate(0,-1));
-      }
-      if (top) {
-        m_missing_tiles.emplace(current_tile + PixelCoordinate(0,1));
-      }
-      if (left && bottom) {
-        m_missing_tiles.emplace(current_tile + PixelCoordinate(-1,-1));
-      }
-      if (left && top) {
-        m_missing_tiles.emplace(current_tile + PixelCoordinate(-1,1));
-      }
-      if (right && bottom) {
-        m_missing_tiles.emplace(current_tile + PixelCoordinate(1,-1));
-      }
-      if (right && top) {
-        m_missing_tiles.emplace(current_tile + PixelCoordinate(1,1));
-      }
-    }
-
-//    void debugPrint() const {
-//      std::cout << "group with " << m_pixels.size() << " pixels and missing: ";
-//      for (auto& pc : m_missing_tiles) {
-//        std::cout << "(" << pc.m_x << ", " << pc.m_y << ") ";
-//      }
-//      std::cout << "\n";
-//    }
+  enum {
+    MISSING_TOP_TILE = 1,
+    MISSING_TOP_RIGHT_TILE = 2,
+    MISSING_RIGHT_TILE = 4,
+    MISSING_BOTTOM_RIGHT_TILE = 8,
+    MISSING_BOTTOM_TILE = 16,
+    MISSING_BOTTOM_LEFT_TILE = 32,
+    MISSING_LEFT_TILE = 64,
+    MISSING_TOP_LEFT_TILE = 128,
   };
 
-  TilesLabellingListener(Segmentation::LabellingListener& listener, std::shared_ptr<SourceFactory> source_factory) :
+
+  struct PartialGroup {
+    std::vector<PixelCoordinate> m_pixels;
+    PixelCoordinate m_tile_coordinate;
+    unsigned int m_missing_tiles;
+
+    PartialGroup() : m_pixels(), m_tile_coordinate(), m_missing_tiles(0) {}
+
+    PartialGroup(const std::vector<PixelCoordinate>& pixels, PixelCoordinate tile_coordinate)
+        : m_pixels(pixels), m_tile_coordinate(tile_coordinate), m_missing_tiles(0) {}
+  };
+
+  TilesLabellingListener(Segmentation::LabellingListener& listener,
+      std::shared_ptr<SourceFactory> source_factory, int tile_width, int tile_height, int width, int height) :
     m_listener(listener),
-    m_source_factory(source_factory) {}
+    m_source_factory(source_factory),
+    m_width(width), m_height(height),
+    m_tile_width(tile_width), m_tile_height(tile_height) {}
 
   virtual ~TilesLabellingListener() = default;
 
+  void startTileSegmentation(PixelCoordinate tile_coordinate) {
+    m_current_tile = tile_coordinate;
+  }
+
+  // Called by the Lutz algorithm for each detection on a tile
   void publishGroup(Lutz::PixelGroup& pixel_group) override {
-    auto group = std::make_shared<PartialGroup>(pixel_group.pixel_list);
+    auto group = std::make_shared<PartialGroup>(pixel_group.pixel_list, m_current_tile);
 
-    group->determineMissingTiles(m_current_tile, m_min_x, m_min_y, m_max_x, m_max_y);
-
-    if (group->m_missing_tiles.empty()) {
+    processGroup(group);
+    if (group->m_missing_tiles == 0) {
       publishSource(group);
     } else {
-      for (auto& pc : group->m_pixels) {
-        m_groups_on_borders[pc] = group;
+    }
+  }
+
+  void processGroup(std::shared_ptr<PartialGroup> group) {
+    for (auto& pc : group->m_pixels) {
+      unsigned int pixel_mask = 0;
+      if (pc.m_x == 0) {
+        pixel_mask |= MISSING_LEFT_TILE;
+      }
+      if (pc.m_y == 0) {
+        pixel_mask |= MISSING_BOTTOM_TILE;
+      }
+      if (pc.m_x == m_tile_width-1) {
+        pixel_mask |= MISSING_RIGHT_TILE;
+      }
+      if (pc.m_y == m_tile_height-1) {
+        pixel_mask |= MISSING_TOP_TILE;
+      }
+      group->m_missing_tiles |= pixel_mask;
+
+      if (pixel_mask != 0) {
+
       }
     }
   }
 
-//  void evaluateTile(PixelCoordinate tile) {
-//    for (auto group : m_incomplete_groups[tile]) {
-//    }
-////    //std::cout << "flush tile:\n";
-////    for (auto group : m_tile_buffer) {
-////      publishSource(group);
-////      for (auto& pc : group->m_pixels) {
-////        m_groups_on_borders.erase(pc);
-////      }
-////    }
-////
-////    m_tile_buffer.clear();
-//  }
+  void endTileSegmentation() {
+    m_visited_tiles.emplace(m_current_tile);
+  }
 
   void publishSource(std::shared_ptr<PartialGroup> group) {
-    auto source = m_source_factory->createSource();
-    source->setProperty<PixelCoordinateList>(group->m_pixels);
-    source->setProperty<SourceId>();
+    std::vector<PixelCoordinate> pixels;
+    PixelCoordinate offset(group->m_tile_coordinate.m_x *  m_tile_width, group->m_tile_coordinate.m_y *  m_tile_height);
+    for (auto& pc : group->m_pixels) {
+      pixels.emplace_back(pc + offset);
+    }
 
-    //std::cout << "publish id: " << source->getProperty<SourceId>().getSourceId() << " " << group->m_pixels.size() << " pixels\n";
+    auto source = m_source_factory->createSource();
+    source->setProperty<PixelCoordinateList>(pixels);
+    source->setProperty<SourceId>();
 
     m_listener.publishSource(source);
   }
 
-//  std::shared_ptr<PartialGroup> mergeGroups(std::set<std::shared_ptr<PartialGroup>> groups) {
-//    auto merged_group = std::make_shared<PartialGroup>();
-//
-//    for (auto& group : groups) {
-//      m_tile_buffer.erase(group);
-//
-//      //std::cout << " merging ";
-//      //group->debugPrint();
-//
-//      merged_group->m_pixels.insert(merged_group->m_pixels.end(),
-//          std::make_move_iterator(group->m_pixels.begin()), std::make_move_iterator(group->m_pixels.end()));
-//
-//      merged_group->m_missing_tiles.insert(group->m_missing_tiles.begin(), group->m_missing_tiles.end());
-//    }
-//
-//    for (auto& pc : merged_group->m_pixels) {
-//      m_groups_on_borders[pc] = merged_group;
-//    }
-//
-//    return merged_group;
-//  }
-
   void notifyProgress(int line, int total) override {
-//    m_listener.notifyProgress(line, total);
-//
-//    if (m_window_size > 0 && line > m_window_size) {
-//      m_listener.requestProcessing(ProcessSourcesEvent(LineSelectionCriteria(line - m_window_size)));
-//    }
+    //m_listener.notifyProgress(line, total);
   }
 
-  void setTileBoundary(PixelCoordinate offset, int width, int height) {
-    m_min_x =  offset.m_x;
-    m_max_x =  offset.m_x + width - 1;
-    m_min_y =  offset.m_y;
-    m_max_y =  offset.m_y + height - 1;
-  }
-
-  void setCurrentTile(PixelCoordinate pc) {
-    //std::cout << "Tile " << pc.m_x << ", " << pc.m_y << "\n";
-    m_current_tile = pc;
-    m_visited_tiles.emplace(pc);
-  }
-
-//  void flush() {
-//    //std::cout << "FLUSHING: \n";
-//    while (m_groups_on_borders.size() > 0) {
-//      auto group = m_groups_on_borders.begin()->second;
-//
-////      group->debugPrint();
-//
-//      publishSource(group);
-//      for (auto& pc : group->m_pixels) {
-//        m_groups_on_borders.erase(pc);
-//      }
-//    }
-//  }
 
 private:
   Segmentation::LabellingListener& m_listener;
   std::shared_ptr<SourceFactory> m_source_factory;
+  int m_width, m_height;
+  int m_tile_width, m_tile_height;
 
-  std::unordered_map<PixelCoordinate, std::shared_ptr<PartialGroup>> m_groups_on_borders;
-
-  int m_min_x, m_min_y;
-  int m_max_x, m_max_y;
 
   PixelCoordinate m_current_tile;
   std::unordered_set<PixelCoordinate> m_visited_tiles;
-
-  std::unordered_map<PixelCoordinate, std::shared_ptr<PartialGroup>> m_incomplete_groups;
-
-  //std::unordered_set<std::shared_ptr<PartialGroup>> m_tile_buffer;
 };
 
 }
+
+//
+// TileBasedSegmentation
+//
 
 void TileBasedSegmentation::labelImage(Segmentation::LabellingListener& listener,
     std::shared_ptr<const DetectionImageFrame> frame) {
   Lutz lutz;
   auto image = frame->getThresholdedImage();
-  TilesLabellingListener tiles_listener(listener, m_source_factory);
 
+  // Mark all tiles surrounding the image as "visited" since they will never be processed and we shouldn't wait for them
   int width_in_tiles = (image->getWidth() / TileManager::getInstance()->getTileWidth());
   int height_in_tiles = (image->getHeight() / TileManager::getInstance()->getTileHeight());
 
-  for (int i=-1; i <= width_in_tiles; i++) {
-    tiles_listener.setCurrentTile(PixelCoordinate(i, -1));
-    tiles_listener.setCurrentTile(PixelCoordinate(i, height_in_tiles));
-  }
-  for (int i=-1; i <= height_in_tiles; i++) {
-    tiles_listener.setCurrentTile(PixelCoordinate(-1, i));
-    tiles_listener.setCurrentTile(PixelCoordinate(width_in_tiles, i));
-  }
+  TilesLabellingListener tiles_listener(listener, m_source_factory,
+      TileManager::getInstance()->getTileWidth(), TileManager::getInstance()->getTileHeight(),
+      width_in_tiles, height_in_tiles);
 
+
+  // Process image tiles in the Hilbert curve order
   for (auto& tile : getTiles(*image)) {
-    tiles_listener.setCurrentTile(tile.coord);
     auto sub_image = SubImage<DetectionImage::PixelType>::create(image, tile.offset, tile.width, tile.height);
-    tiles_listener.setTileBoundary(tile.offset, tile.width, tile.height);
-    lutz.labelImage(tiles_listener, *sub_image, tile.offset);
-//    tiles_listener.evaluateTile();
+
+    tiles_listener.startTileSegmentation(tile.coord);
+    lutz.labelImage(tiles_listener, *sub_image);
+    tiles_listener.endTileSegmentation();
   }
-  //tiles_listener.flush();
 }
 
 std::vector<TileBasedSegmentation::Tile> TileBasedSegmentation::getTiles(const DetectionImage& image) const {
@@ -246,7 +180,6 @@ std::vector<TileBasedSegmentation::Tile> TileBasedSegmentation::getTiles(const D
                       (image.getHeight() + tile_height - 1) / tile_height);
 
   HilbertCurve curve(size);
-
   for (auto& coord : curve.getCurve()) {
     int x = coord.m_x * tile_width;
     int y = coord.m_y * tile_height;
