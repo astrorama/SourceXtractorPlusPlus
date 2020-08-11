71833894504425dc0a35581a94453302292ef945
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-08-11 12:02:39 +0200

    Reduce complexity of FluxMeasurement

--- SEFramework/src/lib/Aperture/FluxMeasurement.cpp
+++ SEFramework/src/lib/Aperture/FluxMeasurement.cpp
@@ -28,6 +28,23 @@ namespace SourceXtractor {
 
 const SeFloat BADAREA_THRESHOLD_APER = 0.1;
 
+static std::tuple<SeFloat, SeFloat>
+getMirrorPixel(SeFloat centroid_x, SeFloat centroid_y, int pixel_x, int pixel_y,
+               const std::shared_ptr<Image<SeFloat>>& img,
+               const std::shared_ptr<Image<SeFloat>>& variance_map,
+               SeFloat variance_threshold) {
+  // get the mirror pixel
+  auto mirror_x = 2 * centroid_x - pixel_x + 0.49999;
+  auto mirror_y = 2 * centroid_y - pixel_y + 0.49999;
+  if (mirror_x >= 0 && mirror_y >= 0 && mirror_x < img->getWidth() && mirror_y < img->getHeight()) {
+    auto variance_tmp = variance_map ? variance_map->getValue(mirror_x, mirror_y) : 1;
+    if (variance_tmp < variance_threshold) {
+      // mirror pixel is OK: take the value
+      return std::make_pair(img->getValue(mirror_x, mirror_y), variance_tmp);
+    }
+  }
+  return std::make_pair(0., 0.);
+}
 
 FluxMeasurement measureFlux(const std::shared_ptr<Aperture> &aperture, SeFloat centroid_x, SeFloat centroid_y,
                             const std::shared_ptr<Image<SeFloat>> &img,
@@ -50,55 +67,42 @@ FluxMeasurement measureFlux(const std::shared_ptr<Aperture> &aperture, SeFloat c
     for (int pixel_x = min_pixel.m_x; pixel_x <= max_pixel.m_x; pixel_x++) {
       SeFloat pixel_value = 0;
       SeFloat pixel_variance = 0;
-      SeFloat variance_tmp = 0;
 
       // get the area coverage and continue if there is overlap
       auto area = aperture->getArea(centroid_x, centroid_y, pixel_x, pixel_y);
-      if (area > 0) {
+      if (area == 0) {
+        continue;
+      }
 
-        measurement.m_total_area += area;
+      measurement.m_total_area += area;
 
-        // make sure the pixel is inside the image
-        if (pixel_x >= 0 && pixel_y >= 0 && pixel_x < img->getWidth() && pixel_y < img->getHeight()) {
+      // make sure the pixel is inside the image
+      if (pixel_x >= 0 && pixel_y >= 0 && pixel_x < img->getWidth() && pixel_y < img->getHeight()) {
 
-          variance_tmp = variance_map ? variance_map->getValue(pixel_x, pixel_y) : 1;
-          if (variance_tmp > variance_threshold) {
-
-            measurement.m_bad_area += 1;
-
-            // try using the mirror pixel
-            if (use_symmetry) {
-              // get the mirror pixel
-              auto mirror_x = 2 * centroid_x - pixel_x + 0.49999;
-              auto mirror_y = 2 * centroid_y - pixel_y + 0.49999;
-              if (mirror_x >= 0 && mirror_y >= 0 && mirror_x < img->getWidth() && mirror_y < img->getHeight()) {
-                variance_tmp = variance_map ? variance_map->getValue(mirror_x, mirror_y) : 1;
-                if (variance_tmp < variance_threshold) {
-                  // mirror pixel is OK: take the value
-                  pixel_value = img->getValue(mirror_x, mirror_y);
-                  pixel_variance = variance_tmp;
-                }
-              }
-            }
-          } else {
-            pixel_value = img->getValue(pixel_x, pixel_y);
-            pixel_variance = variance_tmp;
+        SeFloat variance_tmp = variance_map ? variance_map->getValue(pixel_x, pixel_y) : 1;
+        if (variance_tmp > variance_threshold) {
+          measurement.m_bad_area += 1;
+          if (use_symmetry) {
+            std::tie(pixel_value, pixel_variance) = getMirrorPixel(
+              centroid_x, centroid_y, pixel_x, pixel_y, img, variance_map, variance_threshold);
           }
-
-          measurement.m_flux += pixel_value * area;
-          measurement.m_variance += pixel_variance * area;
         } else {
-          measurement.m_flags |= Flags::BOUNDARY;
+          pixel_value = img->getValue(pixel_x, pixel_y);
+          pixel_variance = variance_tmp;
         }
+
+        measurement.m_flux += pixel_value * area;
+        measurement.m_variance += pixel_variance * area;
+      } else {
+        measurement.m_flags |= Flags::BOUNDARY;
       }
     }
   }
 
   // check/set the bad area flag
-  if (measurement.m_total_area > 0) {
-    if (measurement.m_bad_area / measurement.m_total_area > BADAREA_THRESHOLD_APER) {
-      measurement.m_flags |= Flags::BIASED;
-    }
+  if (measurement.m_total_area > 0 &&
+      measurement.m_bad_area / measurement.m_total_area > BADAREA_THRESHOLD_APER) {
+    measurement.m_flags |= Flags::BIASED;
   }
 
   return measurement;
