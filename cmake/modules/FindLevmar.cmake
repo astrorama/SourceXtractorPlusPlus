7fe2086d8f6b25a036de33e9cdccac978760d02d
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-09-07 13:18:59 +0200

    Fix typos on Find*.cmake modules

--- cmake/modules/FindLevmar.cmake
+++ cmake/modules/FindLevmar.cmake
@@ -1,34 +1,34 @@
 # - Locate the Levmar library
 # Defines:
 #
-#  LEVMAR_FOUND
-#  LEVMAR_INCLUDE_DIR
-#  LEVMAR_INCLUDE_DIRS (not cached)
-#  LEVMAR_LIBRARY
-#  LEVMAR_LIBRARIES (not cached)
+#  Levmar_FOUND
+#  Levmar_INCLUDE_DIR
+#  Levmar_INCLUDE_DIRS (not cached)
+#  Levmar_LIBRARY
+#  Levmar_LIBRARIES (not cached)
 
 
-if(NOT LEVMAR_FOUND)
+if(NOT Levmar_FOUND)
 
-    find_path(LEVMAR_INCLUDE_DIR levmar.h
+    find_path(Levmar_INCLUDE_DIR levmar.h
             HINTS ENV LEVMAR_ROOT_DIR LEVMAR_INSTALL_DIR
             PATH_SUFFIXES include)
 
-    find_library(LEVMAR_LIBRARY levmar
+    find_library(Levmar_LIBRARY levmar
             HINTS ENV LEVMAR_ROOT_DIR LEVMAR_INSTALL_DIR
             PATH_SUFFIXES lib)
 
     find_library(M_LIBRARY m)
 
-    set(LEVMAR_LIBRARIES ${LEVMAR_LIBRARY})
-    set(LEVMAR_INCLUDE_DIRS ${LEVMAR_INCLUDE_DIR})
+    set(Levmar_LIBRARIES ${Levmar_LIBRARY})
+    set(Levmar_INCLUDE_DIRS ${Levmar_INCLUDE_DIR})
 
     INCLUDE(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(LEVMAR DEFAULT_MSG LEVMAR_INCLUDE_DIRS LEVMAR_LIBRARIES)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(Levmar DEFAULT_MSG Levmar_INCLUDE_DIRS Levmar_LIBRARIES)
 
-    mark_as_advanced(LEVMAR_FOUND LEVMAR_INCLUDE_DIRS LEVMAR_LIBRARIES)
+    mark_as_advanced(Levmar_FOUND Levmar_INCLUDE_DIRS Levmar_LIBRARIES)
 
-    list(REMOVE_DUPLICATES LEVMAR_LIBRARIES)
-    list(REMOVE_DUPLICATES LEVMAR_INCLUDE_DIRS)
+    list(REMOVE_DUPLICATES Levmar_LIBRARIES)
+    list(REMOVE_DUPLICATES Levmar_INCLUDE_DIRS)
 
 endif()
