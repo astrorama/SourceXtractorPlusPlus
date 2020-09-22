78800085fde79778ddf2d200e0c5c0b9aac32bba
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-09-22 13:55:16 +0200

    Revert "Fix typos on Find*.cmake modules"
    This reverts commit 7fe2086d8f6b25a036de33e9cdccac978760d02d.
    Fixes build on CentOS7

--- cmake/modules/FindBoostDLL.cmake
+++ cmake/modules/FindBoostDLL.cmake
@@ -1,12 +1,12 @@
-if (NOT BoostDLL_FOUND)
+if (NOT BoostDDL_FOUND)
 
     find_path(BoostDLL_INCLUDE_DIRS boost/dll
               HINTS ENV BOOST_DLL_INSTALL_DIR
               PATH_SUFFIXES include)
 
     include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(BoostDLL DEFAULT_MSG BoostDLL_INCLUDE_DIRS)
-    mark_as_advanced(BoostDLL_FOUND BoostDLL_INCLUDE_DIRS)
+    find_package_handle_standard_args(BoostDDL DEFAULT_MSG BoostDLL_INCLUDE_DIRS)
+    mark_as_advanced(BoostDDL_FOUND BoostDLL_INCLUDE_DIRS)
 
 endif()
 
