5da4788707a924a11fafa071cb7861a14224893b
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-10-12 16:35:20 +0200

    Compile dependent parameters

--- SEImplementation/src/lib/PythonConfig/PythonInterpreter.cpp
+++ SEImplementation/src/lib/PythonConfig/PythonInterpreter.cpp
@@ -55,7 +55,7 @@ PythonInterpreter::PythonInterpreter(): m_out_wrapper(stdout_logger), m_err_wrap
   struct sigaction sigint_handler;
   sigaction(SIGINT, nullptr, &sigint_handler);
 
-  PyImport_AppendInittab("pyston", &PyInit_libPyston);
+  PyImport_AppendInittab("pyston", &PyInit_pyston);
   Py_Initialize();
   PyEval_InitThreads();
   PyEval_SaveThread();
