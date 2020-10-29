20df661c14a57d637f1d1f94df26bd1411e40645
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-10-29 09:41:34 +0100

    Use PYSTON_MODULE_INIT macro
    Keeps compatibility between python2 and 3

--- SEImplementation/src/lib/PythonConfig/PythonInterpreter.cpp
+++ SEImplementation/src/lib/PythonConfig/PythonInterpreter.cpp
@@ -53,7 +53,7 @@ PythonInterpreter::PythonInterpreter(): m_out_wrapper(stdout_logger), m_err_wrap
   struct sigaction sigint_handler;
   sigaction(SIGINT, nullptr, &sigint_handler);
 
-  PyImport_AppendInittab("pyston", &PyInit_libPyston);
+  PyImport_AppendInittab("pyston", PYSTON_MODULE_INIT);
   Py_Initialize();
   PyEval_InitThreads();
   PyEval_SaveThread();
