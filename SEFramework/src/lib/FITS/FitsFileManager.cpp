fd4bd87aff439fc13025504c8325fa191323eedc
Marc Schefer <Marc.Schefer@unige.ch>
2020-11-03 14:57:51 +0100

    fix maximum number of simultaneous files regression

--- SEFramework/src/lib/FITS/FitsFileManager.cpp
+++ SEFramework/src/lib/FITS/FitsFileManager.cpp
@@ -67,8 +67,20 @@ void FitsFileManager::closeExtraFiles() {
   while (m_open_files.size() > m_max_open_files) {
     auto& file_to_close = m_fits_files[m_open_files.back()];
     file_to_close->close();
-    m_open_files.pop_back();
   }
 }
+
+void FitsFileManager::reportClosedFile(const std::string& filename) {
+  m_open_files.remove(filename);
+}
+
+void FitsFileManager::reportOpenedFile(const std::string& filename) {
+  auto iter = std::find(m_open_files.begin(), m_open_files.end(), filename);
+  if (iter == m_open_files.end()) {
+    m_open_files.push_front(filename);
+  }
+  closeExtraFiles();
+}
+
 
 }
