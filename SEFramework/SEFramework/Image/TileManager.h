61b97471c6ecd4ebf3ae22b27b313d2c016269b9
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2020-12-11 14:31:00 +0100

    Move TileManager code to a cpp

--- SEFramework/SEFramework/Image/TileManager.h
+++ SEFramework/SEFramework/Image/TileManager.h
@@ -43,18 +43,12 @@ struct TileKey {
   std::shared_ptr<const ImageSource> m_source;
   int m_tile_x, m_tile_y;
 
-  bool operator==(const TileKey& other) const {
-    return m_source == other.m_source && m_tile_x == other.m_tile_x && m_tile_y == other.m_tile_y;
-  }
+  bool operator==(const TileKey& other) const;
 
-  std::string getRepr() const {
-    std::ostringstream str;
-    str << m_source.get() << "[" << m_source->getRepr() << "] " << m_tile_x << ","  << m_tile_y;
-    return str.str();
-  }
+  std::string getRepr() const;
 };
 
-inline std::ostream& operator << (std::ostream &out, const TileKey &tk) {
+inline std::ostream& operator<<(std::ostream& out, const TileKey& tk) {
   out << tk.getRepr();
   return out;
 }
@@ -63,9 +57,8 @@ inline std::ostream& operator << (std::ostream &out, const TileKey &tk) {
 
 namespace std {
 
-template <>
-struct hash<SourceXtractor::TileKey>
-{
+template<>
+struct hash<SourceXtractor::TileKey> {
   std::size_t operator()(const SourceXtractor::TileKey& key) const {
     std::size_t hash = 0;
     boost::hash_combine(hash, key.m_source);
@@ -82,113 +75,33 @@ namespace SourceXtractor {
 class TileManager {
 public:
 
-  TileManager() : m_tile_width(256), m_tile_height(256), m_max_memory(100 * 1024L * 1024L), m_total_memory_used(0),
-                  m_tile_logger(Elements::Logging::getLogger("TileManager")) {
-  }
+  TileManager();
 
-  virtual ~TileManager() {
-    saveAllTiles();
-  }
+  virtual ~TileManager();
 
   // Actually not thread safe, call before starting the multi-threading
-  void setOptions(int tile_width, int tile_height, int max_memory) {
-    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+  void setOptions(int tile_width, int tile_height, int max_memory);
 
-    flush();
+  void flush();
 
-    m_tile_width = tile_width;
-    m_tile_height = tile_height;
-    m_max_memory = max_memory*1024L*1024L;
-  }
+  std::shared_ptr<ImageTile>
+  getTileForPixel(int x, int y, std::shared_ptr<const ImageSource> source);
 
-  void flush() {
-    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+  static std::shared_ptr<TileManager> getInstance();
 
-    // empty anything still stored in cache
-    saveAllTiles();
-    m_tile_list.clear();
-    m_tile_map.clear();
-    m_total_memory_used = 0;
-  }
+  void saveAllTiles();
 
-  std::shared_ptr<ImageTile> getTileForPixel(int x, int y, std::shared_ptr<const ImageSource> source) {
-    std::lock_guard<std::recursive_mutex> lock(m_mutex);
+  int getTileWidth() const;
 
-    x = x / m_tile_width * m_tile_width;
-    y = y / m_tile_height * m_tile_height;
-
-    TileKey key {std::static_pointer_cast<const ImageSource>(source), x, y};
-    auto it = m_tile_map.find(key);
-    if (it != m_tile_map.end()) {
-#ifndef NDEBUG
-      //m_tile_logger.debug() << "Cache hit " << key;
-#endif
-      return std::dynamic_pointer_cast<ImageTile>(it->second);
-    } else {
-      auto tile = source->getImageTile(x, y,
-          std::min(m_tile_width, source->getWidth()-x), std::min(m_tile_height, source->getHeight()-y));
-      addTile(key, std::static_pointer_cast<ImageTile>(tile));
-      removeExtraTiles();
-      return tile;
-    }
-  }
-
-  static std::shared_ptr<TileManager> getInstance() {
-    if (s_instance == nullptr) {
-      s_instance = std::make_shared<TileManager>();
-    }
-    return s_instance;
-  }
-
-  void saveAllTiles() {
-    std::lock_guard<std::recursive_mutex> lock(m_mutex);
-
-    for (auto tile_key : m_tile_list) {
-      m_tile_map.at(tile_key)->saveIfModified();
-    }
-  }
-
-  int getTileWidth() const {
-    return m_tile_width;
-  }
-
-  int getTileHeight() const {
-    return m_tile_height;
-  }
+  int getTileHeight() const;
 
 private:
 
-  void removeTile(TileKey tile_key) {
-#ifndef NDEBUG
-    //m_tile_logger.debug() << "Cache eviction " << tile_key;
-#endif
+  void removeTile(TileKey tile_key);
 
-    auto& tile = m_tile_map.at(tile_key);
+  void removeExtraTiles();
 
-    tile->saveIfModified();
-    m_total_memory_used -= tile->getTileMemorySize();
-
-    m_tile_map.erase(tile_key);
-  }
-
-  void removeExtraTiles() {
-    while (m_total_memory_used > m_max_memory) {
-      assert(m_tile_list.size() > 0);
-      auto tile_to_remove = m_tile_list.back();
-      removeTile(tile_to_remove);
-      m_tile_list.pop_back();
-    }
-  }
-
-  void addTile(TileKey key, std::shared_ptr<ImageTile> tile) {
-#ifndef NDEBUG
-    //m_tile_logger.debug() << "Cache miss " << key;
-#endif
-
-    m_tile_map[key] = tile;
-    m_tile_list.push_front(key);
-    m_total_memory_used += tile->getTileMemorySize();
-  }
+  void addTile(TileKey key, std::shared_ptr<ImageTile> tile);
 
   int m_tile_width, m_tile_height;
   long m_max_memory;
@@ -199,9 +112,6 @@ private:
 
   std::recursive_mutex m_mutex;
 
-  Elements::Logging m_tile_logger;
-
-  static std::shared_ptr<TileManager> s_instance;
 };
 
 }
--- SEFramework/src/lib/Image/TileManager.cpp
+++ SEFramework/src/lib/Image/TileManager.cpp
@@ -25,6 +25,126 @@
 
 namespace SourceXtractor {
 
-std::shared_ptr<TileManager> TileManager::s_instance;
+static std::shared_ptr<TileManager> s_instance;
+static Elements::Logging s_tile_logger = Elements::Logging::getLogger("TileManager");
+
+bool TileKey::operator==(const TileKey& other) const {
+  return m_source == other.m_source && m_tile_x == other.m_tile_x && m_tile_y == other.m_tile_y;
+}
+
+std::string TileKey::getRepr() const {
+  std::ostringstream str;
+  str << m_source.get() << "[" << m_source->getRepr() << "] " << m_tile_x << "," << m_tile_y;
+  return str.str();
+}
+
+
+TileManager::TileManager() : m_tile_width(256), m_tile_height(256),
+                             m_max_memory(100 * 1024L * 1024L), m_total_memory_used(0) {
+}
+
+TileManager::~TileManager() {
+  saveAllTiles();
+}
+
+void TileManager::setOptions(int tile_width, int tile_height, int max_memory) {
+  std::lock_guard<std::recursive_mutex> lock(m_mutex);
+
+  flush();
+
+  m_tile_width = tile_width;
+  m_tile_height = tile_height;
+  m_max_memory = max_memory * 1024L * 1024L;
+}
+
+void TileManager::flush() {
+  std::lock_guard<std::recursive_mutex> lock(m_mutex);
+
+  // empty anything still stored in cache
+  saveAllTiles();
+  m_tile_list.clear();
+  m_tile_map.clear();
+  m_total_memory_used = 0;
+}
+
+std::shared_ptr<ImageTile> TileManager::getTileForPixel(int x, int y,
+                                                        std::shared_ptr<const ImageSource> source) {
+  std::lock_guard<std::recursive_mutex> lock(m_mutex);
+
+  x = x / m_tile_width * m_tile_width;
+  y = y / m_tile_height * m_tile_height;
+
+  TileKey key{std::static_pointer_cast<const ImageSource>(source), x, y};
+  auto it = m_tile_map.find(key);
+  if (it != m_tile_map.end()) {
+#ifndef NDEBUG
+    s_tile_logger.debug() << "Cache hit " << key;
+#endif
+    return std::dynamic_pointer_cast<ImageTile>(it->second);
+  }
+  else {
+    auto tile = source->getImageTile(x, y,
+                                     std::min(m_tile_width, source->getWidth() - x),
+                                     std::min(m_tile_height, source->getHeight() - y));
+    addTile(key, std::static_pointer_cast<ImageTile>(tile));
+    removeExtraTiles();
+    return tile;
+  }
+}
+
+std::shared_ptr<TileManager> TileManager::getInstance() {
+  if (s_instance == nullptr) {
+    s_instance = std::make_shared<TileManager>();
+  }
+  return s_instance;
+}
+
+void TileManager::saveAllTiles() {
+  std::lock_guard<std::recursive_mutex> lock(m_mutex);
+
+  for (auto tile_key : m_tile_list) {
+    m_tile_map.at(tile_key)->saveIfModified();
+  }
+}
+
+int TileManager::getTileWidth() const {
+  return m_tile_width;
+}
+
+int TileManager::getTileHeight() const {
+  return m_tile_height;
+}
+
+void TileManager::removeTile(TileKey tile_key) {
+#ifndef NDEBUG
+  s_tile_logger.debug() << "Cache eviction " << tile_key;
+#endif
+
+  auto& tile = m_tile_map.at(tile_key);
+
+  tile->saveIfModified();
+  m_total_memory_used -= tile->getTileMemorySize();
+
+  m_tile_map.erase(tile_key);
+}
+
+void TileManager::removeExtraTiles() {
+  while (m_total_memory_used > m_max_memory) {
+    assert(m_tile_list.size() > 0);
+    auto tile_to_remove = m_tile_list.back();
+    removeTile(tile_to_remove);
+    m_tile_list.pop_back();
+  }
+}
+
+void TileManager::addTile(TileKey key, std::shared_ptr<ImageTile> tile) {
+#ifndef NDEBUG
+  s_tile_logger.debug() << "Cache miss " << key;
+#endif
+
+  m_tile_map[key] = tile;
+  m_tile_list.push_front(key);
+  m_total_memory_used += tile->getTileMemorySize();
+}
 
 }
