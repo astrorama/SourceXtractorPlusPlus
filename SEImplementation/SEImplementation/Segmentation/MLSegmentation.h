287f0c236ecf033c6d5a35f39dfc9972a9f6f19b
Marc Schefer <Marc.Schefer@unige.ch>
2021-01-18 16:39:49 +0100

    created class MLSegementation

---
+++ SEImplementation/SEImplementation/Segmentation/MLSegmentation.h
@@ -0,0 +1,59 @@
+/** Copyright © 2019 Université de Genève, LMU Munich - Faculty of Physics, IAP-CNRS/Sorbonne Université
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+
+#ifndef _SEIMPLEMENTATION_SEGMENTATION_MLSEGMENTATION_H_
+#define _SEIMPLEMENTATION_SEGMENTATION_MLSEGMENTATION_H_
+
+#include "SEFramework/Source/SourceFactory.h"
+#include "SEFramework/Pipeline/Segmentation.h"
+
+namespace SourceXtractor {
+
+
+/**
+ * @class MLSegmentation
+ * @brief Implements a Segmentation based on the BFS algorithm
+ */
+class MLSegmentation : public Segmentation::Labelling {
+public:
+
+  virtual ~MLSegmentation() = default;
+
+  MLSegmentation(std::shared_ptr<SourceFactory> source_factory, int max_delta)
+      : m_source_factory(source_factory), m_max_delta(max_delta) {
+    assert(source_factory != nullptr);
+  }
+
+  void labelImage(Segmentation::LabellingListener& listener, std::shared_ptr<const DetectionImageFrame> frame) override;
+
+private:
+  struct Tile {
+    PixelCoordinate offset;
+    int width, height;
+  };
+
+  std::vector<MLSegmentation::Tile> getTiles(const DetectionImage& image) const;
+
+
+  std::shared_ptr<SourceFactory> m_source_factory;
+  int m_max_delta;
+};
+
+}
+
+#endif /* _SEIMPLEMENTATION_SEGMENTATION_MLSEGMENTATION_H_ */
