17b7cee7c624fc14b45a5540a6b218ea82643867
Marc Schefer <Marc.Schefer@unige.ch>
2021-01-18 16:39:49 +0100

    created class MLSegementation

---
+++ SEImplementation/src/lib/Segmentation/MLSegmentation.cpp
@@ -0,0 +1,38 @@
+/** Copyright © 2019 Université de Genève, LMU Munich - Faculty of Physics, IAP-CNRS/Sorbonne Université
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <memory>
+#include <vector>
+#include <list>
+#include <iostream>
+
+#include "SEUtils/PixelCoordinate.h"
+#include "SEUtils/HilbertCurve.h"
+
+#include "SEFramework/Frame/Frame.h"
+#include "SEFramework/Image/TileManager.h"
+
+#include "SEImplementation/Property/PixelCoordinateList.h"
+#include "SEImplementation/Property/SourceId.h"
+
+#include "SEImplementation/Measurement/MultithreadedMeasurement.h"
+
+#include "SEImplementation/Segmentation/MLSegmentation.h"
+
+namespace SourceXtractor {
+
+}
