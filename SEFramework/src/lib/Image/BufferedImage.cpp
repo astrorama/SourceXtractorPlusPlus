1bdb54d864b42f3db09430f26884683bac84175c
Marc Schefer <Marc.Schefer@unige.ch>
2021-02-22 17:57:35 +0100

    ImageTile performance improvement and possible crash fix

--- SEFramework/src/lib/Image/BufferedImage.cpp
+++ SEFramework/src/lib/Image/BufferedImage.cpp
@@ -78,12 +78,12 @@ std::shared_ptr<ImageChunk<T>> BufferedImage<T>::getChunk(int x, int y, int widt
     // the tile image is going to be kept in memory as long as the chunk exists, but it could be unloaded
     // from TileManager and even reloaded again, wasting memory,
     // however image chunks are normally short lived so it's probably OK
-    auto tile = m_tile_manager->getTileForPixel(x, y, m_source);
-    // The tile may be smaller than tile_width x tile_height if the image is smaller, or does not divide neatly!
+    auto tile = std::dynamic_pointer_cast<ImageTileWithType<T>>(m_tile_manager->getTileForPixel(x, y, m_source));
+    assert(tile != nullptr);
 
-    auto image = tile->getImage<T>();
-    const T *data_start = &(image->getData()[tile_offset_x +
-                                                        tile_offset_y * image->getWidth()]);
+    // The tile may be smaller than tile_width x tile_height if the image is smaller, or does not divide neatly!
+    auto image = tile->getImage();
+    const T *data_start = &(image->getData()[tile_offset_x + tile_offset_y * image->getWidth()]);
     return ImageChunk<T>::create(data_start, width, height, image->getWidth(), image);
   }
   else {
