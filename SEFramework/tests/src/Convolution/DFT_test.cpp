d0be9faa233ceae3341ea1b3ed215362eea70e6d
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2021-06-18 15:09:13 +0200

    In place DFT

--- SEFramework/tests/src/Convolution/DFT_test.cpp
+++ SEFramework/tests/src/Convolution/DFT_test.cpp
@@ -18,10 +18,10 @@
 // Created by aalvarez on 8/17/18.
 //
 
-#include <boost/test/unit_test.hpp>
-#include "SEFramework/Image/VectorImage.h"
 #include "SEFramework/Convolution/DFT.h"
-#include "SEUtils/IsClose.h"
+#include "SEFramework/Image/VectorImage.h"
+#include <SEUtils/TestUtils.h>
+#include <boost/test/unit_test.hpp>
 
 using namespace SourceXtractor;
 
@@ -62,15 +62,30 @@ BOOST_FIXTURE_TEST_CASE ( Convolve_test, DFT_Fixture ) {
     0.0, 0.0, 0.0, 0.0, 0.0,
   });
 
-  for (auto x = 0; x < expected->getWidth(); ++x) {
-    for (auto y = 0; y < expected->getHeight(); ++y) {
-      auto ev = expected->getValue(x, y);
-      auto iv = image->getValue(x, y);
-      if (!isClose(ev, iv, 1e-5, 1e-4)) {
-        BOOST_ERROR("Mismatch at " << x << 'x' << y << ": " << ev << " != " << iv);
-      }
-    }
-  }
+  BOOST_CHECK(compareImages(expected, image, 1e-5, 1e-4));
+}
+
+BOOST_FIXTURE_TEST_CASE ( ConvolveEven_test, DFT_Fixture ) {
+  auto image = VectorImage<SeFloat>::create(6, 5, std::vector<float>{
+      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+      0.0, 1.0, 0.0, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+  });
+
+
+  dft.convolve(image);
+
+  auto expected = VectorImage<SeFloat>::create(6, 5, std::vector<float>{
+      1.0, 2.0, 3.0, 0.0, 0.0, 0.0,
+      4.0, 5.0, 6.0, 0.0, 0.0, 0.0,
+      7.0, 8.0, 9.0, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+  });
+
+  BOOST_CHECK(compareImages(expected, image, 1e-5, 1e-4));
 }
 
 
@@ -90,15 +105,7 @@ BOOST_FIXTURE_TEST_CASE ( Convolve_not_squared_test, DFT_Fixture ) {
     0.0,  7.0,  8.0, 9.0, 0.0,
   });
 
-  for (auto x = 0; x < expected->getWidth(); ++x) {
-    for (auto y = 0; y < expected->getHeight(); ++y) {
-      auto ev = expected->getValue(x, y);
-      auto iv = image->getValue(x, y);
-      if (!isClose(ev, iv, 1e-5, 1e-4)) {
-        BOOST_ERROR("Mismatch at " << x << 'x' << y << ": " << ev << " != " << iv);
-      }
-    }
-  }
+  BOOST_CHECK(compareImages(expected, image, 1e-5, 1e-4));
 }
 
 BOOST_AUTO_TEST_SUITE_END ()
