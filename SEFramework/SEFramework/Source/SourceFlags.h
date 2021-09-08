c15063b113cdadc542f68249ce02384d038c2a77
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2021-09-08 17:08:15 +0200

    Allocate levmar buffer on our side (#394)
    * LevmarEngine: Allocate workarea ourselves
    
    Limit by default to 2 GiB, flag allocation issues.
    
    * Bump sentinel
    
    Co-authored-by: Marc Schefer <31961290+marcschefer@users.noreply.github.com>

--- SEFramework/SEFramework/Source/SourceFlags.h
+++ SEFramework/SEFramework/Source/SourceFlags.h
@@ -45,6 +45,7 @@ enum class Flags : int64_t {
   PARTIAL_FIT       = 1ll << 6,  ///< Some/all of the model parameters could not be fitted
   INSUFFICIENT_DATA = 1ll << 7,  ///< There are not enough good pixels to fit the parameters
   ERROR             = 1ll << 10, ///< Error flag: something bad happened during the measurement, model fitting, etc.
+  MEMORY            = 1ll << 11, ///< Failed to allocate an object, buffer, etc.
   BAD_PROJECTION    = 1ll << 12, ///< Failed to project some of the coordinates into one of the measurement frames
   SENTINEL          = 1ll << 13, ///< Used to find the boundary of possible values
 };
@@ -59,7 +60,8 @@ const std::map<Flags, std::string> FlagsStr = {
   {Flags::OUTSIDE, "OUTSIDE"},
   {Flags::PARTIAL_FIT, "PARTIAL_FIT"},
   {Flags::INSUFFICIENT_DATA, "INSUFFICIENT_DATA"},
-  {Flags::ERROR, "ERROR"}
+  {Flags::ERROR, "ERROR"},
+  {Flags::MEMORY, "MEMORY"}
 };
 
 
