44206041b6df51d7268631b5a82a93e2cec7c2de
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-09-09 11:21:04 +0200

    Expose detection stamp

--- SEImplementation/src/lib/Plugin/DetectionFrameSourceStamp/DetectionFrameSourceStampPlugin.cpp
+++ SEImplementation/src/lib/Plugin/DetectionFrameSourceStamp/DetectionFrameSourceStampPlugin.cpp
@@ -1,4 +1,5 @@
-/** Copyright © 2019 Université de Genève, LMU Munich - Faculty of Physics, IAP-CNRS/Sorbonne Université
+/**
+ * Copyright © 2019-2022 Université de Genève, LMU Munich - Faculty of Physics, IAP-CNRS/Sorbonne Université
  *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -21,26 +22,51 @@
  *      Author: mschefer
  */
 
+#include "SEImplementation/Plugin/DetectionFrameSourceStamp/DetectionFrameSourceStampPlugin.h"
+#include "NdArray/Borrowed.h"
 #include "SEFramework/Plugin/StaticPlugin.h"
-
 #include "SEImplementation/Plugin/DetectionFrameSourceStamp/DetectionFrameSourceStamp.h"
 #include "SEImplementation/Plugin/DetectionFrameSourceStamp/DetectionFrameSourceStampTaskFactory.h"
 
-#include "SEImplementation/Plugin/DetectionFrameSourceStamp/DetectionFrameSourceStampPlugin.h"
-
 namespace SourceXtractor {
+
+using Euclid::NdArray::BorrowedRange;
+using Euclid::NdArray::NdArray;
 
 static StaticPlugin<DetectionFrameSourceStampPlugin> detection_frame_source_stamp_plugin;
 
+struct StampConverter {
+  using DetectionVectorImage = DetectionFrameSourceStamp::DetectionVectorImage;
+  using PixelType            = DetectionVectorImage::PixelType;
+
+  std::function<const DetectionVectorImage&(const DetectionFrameSourceStamp&)> m_getter;
+
+  NdArray<PixelType> operator()(const DetectionFrameSourceStamp& property) const {
+    auto&               stamp = m_getter(property);
+    auto&               data  = const_cast<std::vector<PixelType>&>(stamp.getData());
+    std::vector<size_t> shape{static_cast<size_t>(stamp.getHeight()), static_cast<size_t>(stamp.getWidth())};
+    return NdArray<DetectionImage::PixelType>(shape, BorrowedRange<PixelType>{data.data(), data.size()});
+  }
+};
+
 void DetectionFrameSourceStampPlugin::registerPlugin(PluginAPI& plugin_api) {
-  plugin_api.getTaskFactoryRegistry().registerTaskFactory<DetectionFrameSourceStampTaskFactory, DetectionFrameSourceStamp>();
+  using PixelType = DetectionImage::PixelType;
+
+  plugin_api.getOutputRegistry().registerColumnConverter<DetectionFrameSourceStamp, NdArray<PixelType>>(
+      "detection_stamp", StampConverter{&DetectionFrameSourceStamp::getStamp});
+  plugin_api.getOutputRegistry().registerColumnConverter<DetectionFrameSourceStamp, NdArray<PixelType>>(
+      "detection_filtered_stamp", StampConverter{&DetectionFrameSourceStamp::getFilteredStamp});
+  plugin_api.getOutputRegistry().registerColumnConverter<DetectionFrameSourceStamp, NdArray<PixelType>>(
+      "detection_variance_stamp", StampConverter{&DetectionFrameSourceStamp::getVarianceStamp});
+  plugin_api.getOutputRegistry().registerColumnConverter<DetectionFrameSourceStamp, NdArray<PixelType>>(
+      "detection_thresholded_stamp", StampConverter{&DetectionFrameSourceStamp::getThresholdedStamp});
+
+  plugin_api.getTaskFactoryRegistry()
+      .registerTaskFactory<DetectionFrameSourceStampTaskFactory, DetectionFrameSourceStamp>();
 }
 
 std::string DetectionFrameSourceStampPlugin::getIdString() const {
   return "DetectionFrameSourceStamp";
 }
 
-}
-
-
-
+}  // namespace SourceXtractor
