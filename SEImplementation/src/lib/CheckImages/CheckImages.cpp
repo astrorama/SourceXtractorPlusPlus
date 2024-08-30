2a60a3bd9c62dba6859f909f81b10ac67a88b2e4
Marc Schefer <Marc.Schefer@unige.ch>
2024-08-30 15:15:16 +0200

    fix use of deprecated function

--- SEImplementation/src/lib/CheckImages/CheckImages.cpp
+++ SEImplementation/src/lib/CheckImages/CheckImages.cpp
@@ -156,7 +156,7 @@ void CheckImages::configure(Euclid::Configuration::ConfigManager& manager) {
   const auto& frames = manager.getConfiguration<MeasurementFrameConfig>().getFrames();
   for (auto& info : measurement_images_info) {
     std::stringstream label;
-    label << boost::filesystem::basename(info.m_path) << "_" << info.m_image_hdu;
+    label << boost::filesystem::path(info.m_path).stem().string() << "_" << info.m_image_hdu;
     if (info.m_is_data_cube) {
       label << "_" << info.m_image_layer;
     }
@@ -388,6 +388,7 @@ void CheckImages::saveImages() {
       auto filename = m_residual_filename.stem();
       filename += "_" + frame_info.m_label;
       filename += m_residual_filename.extension();
+
       auto frame_filename = m_residual_filename.parent_path() / filename;
       FitsWriter::writeFile(*residual_image, frame_filename.native(), frame_info.m_coordinate_system);
     }
